217a1d07f37213b8a4af06e660ea74f70b4c82c4
Mikita Aleksandrovich <45469739+MikitaAleksandrovich@users.noreply.github.com>
2019-02-17 20:18:34 +0300

    Add changes to feedback.js using jQuery

--- feedback.js
+++ feedback.js
@@ -1,9 +1,9 @@
-
+ 
   function clearFields ()  {
     
     var fields, fieldsArr;
 
-    fields = document.querySelectorAll('#name' + ', ' + '#email' + ', ' + '#message');
+    fields = $('#name' + ', ' + '#email' + ', ' + '#message');
 
     fieldsArr = Array.from(fields);
     fieldsArr.forEach(function (current) {
@@ -22,7 +22,7 @@ function addBorder () {
         }
     };
 
-    fields = document.querySelectorAll('#name' + ', ' + '#email' + ', ' + '#message');
+    fields = $('#name' + ', ' + '#email' + ', ' + '#message');
 
     nodeListForEach(fields, function(current) {
         current.classList.toggle('orange-focus');
@@ -32,12 +32,12 @@ function addBorder () {
 addBorder();
 
 
-    document.querySelector('.send__btn').addEventListener('click', function () {
-        clearFields();
-    });
+$('.send__btn').click(function () {
+    clearFields();
+});
 
-    document.addEventListener('keypress', function (event) {
-        if (event.keyCode === 13 || event.which === 13) {
-            clearFields ();
-        }
-    });  
+$(document).keypress(function (event) {
+    if (event.keyCode === 13 || event.which === 13) {
+        clearFields ();
+    }
+});  
