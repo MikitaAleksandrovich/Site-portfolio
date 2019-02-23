7638557cd9a47918507e299ae6ed9108ba8f5988
Mikita Aleksandrovich <45469739+MikitaAleksandrovich@users.noreply.github.com>
2019-02-23 13:37:51 +0300

    Add up button
    add up button to faster return to the head of the home page

--- home.js
+++ home.js
@@ -1,1 +1,13 @@
-alert('Hello');
+$(function() {
+  $(window).scroll(function() {
+    if($(this).scrollTop() != 0) {
+      $('#toTop').fadeIn();
+    } else {
+      $('#toTop').fadeOut();
+  }
+});
+  
+$('#toTop').click(function() {
+  $('body,html').animate({scrollTop:0},800);
+});
+});
