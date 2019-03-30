55f28cce58ec6214078cf50cbc151f1cb60768de
MikitaAleksandrovich <aleksandrovich.mikita@gmail.com>
2019-03-30 10:47:01 +0300

    Correct send.php form

--- send.php
+++ send.php
@@ -1,37 +1,25 @@
-<html>
-<head>
-<title>Комментарии с сайта</title>
-</head>
-<body>
-<?php
-//Check, if there are variables in post array
-if(!isset($_POST['name']) and !isset($_POST['email'])) {
- ?> 
-    <form action="send.php" method="post">
-    <input type="text" id="name" name="name" placeholder="Введите ваше имя" required />
-    <input type="email" id="email" name="email" placeholder="E-mail" required />
-    <input type="submit" class ="send__btn" value="Отправить" id="send" name="send"/>
-    </form> 
-<?php } else {
-    //show Form
+<?php 
     $name = $_POST['name'];
-     $email = $_POST['email'];
-     $name = htmlspecialchars($name);
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    $name = htmlspecialchars($name);
     $email = htmlspecialchars($email);
+    $message = htmlspecialchars($message);
     $name = urldecode($name);
     $email = urldecode($email);
+    $messsage = urldecode($message);
     $name = trim($name);
     $email = trim($email);
-if (mail("olia.danilevich@gmail.com", "Комментарий с сайта", "Имя:".$name.". E-mail: ".$email ,"From: example@mail.ru \r\n")){ 
+    $message = trim($message);
+if (mail("olia.danilevich@gmail.com", "Комментарий с сайта", "Имя:".$name.". E-mail: ".$email ,". Сообщение: ".$message ,". From: aleksandrovich.mikita@gmail.ru \r\n") { 
     echo "Сообщение успешно отправлено"; 
  } else { 
     echo "При отправке сообщения возникли ошибки";
-    }
 }
 
 ini_set('display_errors','On');
 error_reporting('E_ALL');
+
 ?>
-</body>
-</html>
+
 
